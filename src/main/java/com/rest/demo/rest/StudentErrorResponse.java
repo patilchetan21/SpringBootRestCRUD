49c0b06f50f99e9edbaf0666d06f5e52aa4ff43f
Chetan Patil <patilchetan2110@gmail.com>
2025-03-23 15:03:30 +0530

    Added Exception handling

---
+++ src/main/java/com/rest/demo/rest/StudentErrorResponse.java
@@ -0,0 +1,40 @@
+package com.rest.demo.rest;
+
+public class StudentErrorResponse {
+    private int status;
+    private long timeStamp;
+    private String message;
+
+    public StudentErrorResponse(int status, long timeStamp, String message) {
+        this.status = status;
+        this.timeStamp = timeStamp;
+        this.message = message;
+    }
+
+    public StudentErrorResponse() {
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
